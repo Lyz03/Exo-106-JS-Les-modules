a1546ed4117f57d54433fbe6dd0a3a956497da93
Lyz03 <lizoe@netcourrier.com>
2021-12-15 16:04:53 +0100

    exo 106

---
+++ HTML.js
@@ -0,0 +1,23 @@
+export {createDiv, createInput, createLabel}
+
+function createDiv(innerText) {
+    let div = document.createElement('div');
+    div.innerText = innerText
+    return div
+}
+
+function createInput(inputType, inputValue, inputId) {
+    let input = document.createElement('input');
+    input.type = inputType;
+    input.value = inputValue
+    input.id = inputId;
+    return input
+
+}
+
+function createLabel(labelFor, labelValue) {
+    let label = document.createElement('label');
+    label.for = labelFor;
+    label.value = labelValue;
+    return label
+}
---
+++ app.js
@@ -0,0 +1,18 @@
+import {createDiv, createInput, createLabel} from "./HTML"
+
+const result = document.getElementById('result')
+
+let newDiv = createDiv('Mon nouveau div');
+result.appendChild(newDiv);
+
+let newLabel = createLabel("input", "Email :");
+result.appendChild(newLabel);
+
+let newInput = createInput('email', "value", "input");
+result.appendChild(newInput);
+
+let newLabel2 = createLabel("input2", "Password :");
+result.appendChild(newLabel2);
+
+let newInput2 = createInput('password', "value", "input2");
+result.appendChild(newInput2);
